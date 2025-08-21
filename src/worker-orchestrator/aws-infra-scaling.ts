bd5e9f1d9610514eb30f64307fc9289afc00ee87
gopala <gopal1852000@gmail.com>
2025-08-21 13:10:01 +0530

    basic set up done with aws api

--- src/worker-orchestrator/aws-infra-scaling.ts
+++ src/worker-orchestrator/aws-infra-scaling.ts
@@ -1,4 +1,32 @@
-//get access to aws account
-//make changes to the desired capicity to machine
+import { AutoScalingClient, DescribeAccountLimitsCommand } from '@aws-sdk/client-auto-scaling';
+import { error } from 'console';
+import dotenv from 'dotenv';
 
-//get machine
+
+dotenv.config()
+
+if(process.env.AWS_ACCESS_KEY == undefined|| process.env.AWS_ACCESS_SECRET == undefined){
+    throw error("env not loaded")
+}
+
+const input = { 
+    region: "us-west-1",
+    credentials: {
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_ACCESS_SECRET,
+    },
+}
+
+const client = new AutoScalingClient(input);
+
+async function send_data(){
+    const input_param = {};
+    const command = new DescribeAccountLimitsCommand(input_param) ;
+    const response = await client.send(command);
+    console.log(response)
+
+}
+
+send_data()
+
+
